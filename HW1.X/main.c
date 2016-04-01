a8b0accb2c52c678b760f076622a7724fd147f80
ayang199 <angelayang2012@u.northwestern.edu>
2016-03-31 21:21:26 -0500

    DEVCFG and setup
    Set the DEVCFG registers, included xc.h and set up the void main()
    function

---
+++ HW1.X/main.c
@@ -0,0 +1,20 @@
+#include <xc.h>
+
+#pragma config FNOSC = FRCPLL
+#pragma config FSOSCEN = ON
+#pragma config POSCMOD = EC
+#pragma config OSCIOFNC = ON
+#pragma config FPBDIV = DIV_2
+
+#pragma config FPLLIDIV = DIV_2
+#pragma config FPLLMUL = MUL_24
+#pragma config FPLLODIV = DIV_2
+#pragma config UPLLIDIV = DIV_2
+#pragma config UPLLEN = ON
+
+void main() {
+    while(1){
+        
+    }
+}
+

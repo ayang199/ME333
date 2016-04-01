57651b978bbb7aca6aa7612cb8d5177ada803940
ayang199 <angelayang2012@u.northwestern.edu>
2016-03-31 21:42:27 -0500

    Delay function and toggle
    Added a delay function and toggle function. The toggle function toggles
    A4 (the LED) while the delay function waits 1/1000 seconds between each
    toggle. Copied from ME333 assignment.

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -12,9 +12,24 @@
 #pragma config UPLLIDIV = DIV_2
 #pragma config UPLLEN = ON
 
+#define DELAYTIME 48000
+void delay(void);
+void toggleLight(void);
+
 void main() {
     while(1){
-        
+        delay(); // delays by 48000 cycles, or 1/1000 secs, meaning the light will toggle at 1000kHz
+        toggleLight();
     }
 }
 
+void delay(void) {
+  int i;
+  for (i = 0; i < DELAYTIME; i++) {
+      ; //do nothing
+  }
+}
+
+void toggleLight(void) {
+  LATAINV = 0b10000; // invert the LED (which is on port A4)
+}

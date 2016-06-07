97b3a87104cbb915f21f86d93bc53d699efb9e14
ayang199 <angelayang2012@u.northwestern.edu>
2016-06-06 21:27:45 -0500

    HW15 updated

--- HW14/usb-serial-for-android-master/usb-serial-for-android-master/usbSerialExamples/src/main/java/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
+++ HW14/usb-serial-for-android-master/usb-serial-for-android-master/usbSerialExamples/src/main/java/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
@@ -32,9 +32,19 @@ import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ScrollView;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
 
 
 import com.hoho.android.usbserial.driver.UsbSerialPort;
@@ -53,10 +63,18 @@ import java.util.concurrent.Executors;
  *
  * @author mike wakerly (opensource@hoho.com)
  */
-public class SerialConsoleActivity extends Activity {
+public class SerialConsoleActivity extends Activity implements TextureView.SurfaceTextureListener {
 
-    SeekBar myControl;
-    TextView myTextView;
+    private Camera mCamera;
+    private TextureView mTextureView;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Bitmap bmp = Bitmap.createBitmap(640,480,Bitmap.Config.ARGB_8888);
+    private Canvas canvas = new Canvas(bmp);
+    private Paint paint1 = new Paint();
+    private TextView mTextView;
+
+    static long prevtime = 0; // for FPS calculation
 
     private final String TAG = SerialConsoleActivity.class.getSimpleName();
 
@@ -106,36 +124,193 @@ public class SerialConsoleActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serial_console);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        mTitleTextView = (TextView) findViewById(R.id.demoTitle);
-        mDumpTextView = (TextView) findViewById(R.id.consoleText);
-        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
-        chkDTR = (CheckBox) findViewById(R.id.checkBoxDTR);
-        chkRTS = (CheckBox) findViewById(R.id.checkBoxRTS);
 
-        myControl = (SeekBar) findViewById(R.id.seekBar);
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        mSurfaceHolder = mSurfaceView.getHolder();
 
-        myTextView = (TextView) findViewById(R.id.textView01);
-        myTextView.setText("Move slider to change LED brightness");
+        mTextureView = (TextureView) findViewById(R.id.textureview);
+        mTextureView.setSurfaceTextureListener(this);
 
+        mTextView = (TextView) findViewById(R.id.cameraStatus);
 
-        chkDTR.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                try {
-                    sPort.setDTR(isChecked);
-                }catch (IOException x){}
+        paint1.setColor(0xffff0000); // red
+        paint1.setTextSize(24);
+
+    }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(640, 480);
+        parameters.setColorEffect(Camera.Parameters.EFFECT_MONO); // black and white
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY); // no autofocusing
+        mCamera.setParameters(parameters);
+        mCamera.setDisplayOrientation(90); // rotate to portrait mode
+
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    // the important function
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Invoked every time there's a new Camera preview frame
+        mTextureView.getBitmap(bmp);
+
+        final Canvas c = mSurfaceHolder.lockCanvas();
+        if (c != null) {
+
+            int[] pixels = new int[bmp.getWidth()];
+            int startY = 15; // which row in the bitmap to analyse to read
+            // only look at one row in the image
+            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1); // (array name, offset inside array, stride (size of row), start x, start y, num pixels to read per row, num rows to read)
+
+            // pixels[] is the RGBA data (in black an white).
+            // instead of doing center of mass on it, decide if each pixel is dark enough to consider black or white
+            // then do a center of mass on the thresholded array
+            int[] thresholdedPixels = new int[bmp.getWidth()];
+            int wbTotal = 0; // total mass
+            int wbCOM = 0; // total (mass time position)
+            for (int i = 0; i < bmp.getWidth(); i++) {
+                // sum the red, green and blue, subtract from 255 to get the darkness of the pixel.
+                // if it is greater than some value (600 here), consider it black
+                // play with the 600 value if you are having issues reliably seeing the line
+                if (255*3-(red(pixels[i])+green(pixels[i])+blue(pixels[i])) > 600) {
+                    thresholdedPixels[i] = 255*3;
+                }
+                else {
+                    thresholdedPixels[i] = 0;
+                }
+                wbTotal = wbTotal + thresholdedPixels[i];
+                wbCOM = wbCOM + thresholdedPixels[i]*i;
             }
-        });
-
-        chkRTS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                try {
-                    sPort.setRTS(isChecked);
-                }catch (IOException x){}
+            int COM;
+            //watch out for divide by 0
+            if (wbTotal<=0) {
+                COM = bmp.getWidth()/2;
             }
-        });
+            else {
+                COM = wbCOM/wbTotal;
+            }
 
+            // draw a circle where you think the COM is
+            canvas.drawCircle(COM, startY, 5, paint1);
+            //***** End of dot 1*****
+
+            //***** Start of dot 2 *****
+            int[] pixels2 = new int[bmp.getWidth()];
+            int startY2 = 200; // which row in the bitmap to analyse to read
+            // only look at one row in the image
+            bmp.getPixels(pixels2, 0, bmp.getWidth(), 0, startY2, bmp.getWidth(), 1); // (array name, offset inside array, stride (size of row), start x, start y, num pixels to read per row, num rows to read)
+
+            // pixels[] is the RGBA data (in black an white).
+            // instead of doing center of mass on it, decide if each pixel is dark enough to consider black or white
+            // then do a center of mass on the thresholded array
+            int[] thresholdedPixels2 = new int[bmp.getWidth()];
+            int wbTotal2 = 0; // total mass
+            int wbCOM2 = 0; // total (mass time position)
+            for (int i = 0; i < bmp.getWidth(); i++) {
+                // sum the red, green and blue, subtract from 255 to get the darkness of the pixel.
+                // if it is greater than some value (600 here), consider it black
+                // play with the 600 value if you are having issues reliably seeing the line
+                if (255*3-(red(pixels2[i])+green(pixels2[i])+blue(pixels2[i])) > 600) {
+                    thresholdedPixels2[i] = 255*3;
+                }
+                else {
+                    thresholdedPixels2[i] = 0;
+                }
+                wbTotal2 = wbTotal2 + thresholdedPixels2[i];
+                wbCOM2 = wbCOM2 + thresholdedPixels2[i]*i;
+            }
+            int COM2;
+            //watch out for divide by 0
+            if (wbTotal2<=0) {
+                COM2 = bmp.getWidth()/2;
+            }
+            else {
+                COM2 = wbCOM2/wbTotal2;
+            }
+
+            // draw a circle where you think the COM is
+            canvas.drawCircle(COM2, startY2, 5, paint1);
+            //************ End of dot 2 *****
+
+
+            //************** Start of dot 3 *********
+            int[] pixels3 = new int[bmp.getWidth()];
+            int startY3 = 400; // which row in the bitmap to analyse to read
+            // only look at one row in the image
+            bmp.getPixels(pixels3, 0, bmp.getWidth(), 0, startY3, bmp.getWidth(), 1); // (array name, offset inside array, stride (size of row), start x, start y, num pixels to read per row, num rows to read)
+
+            // pixels[] is the RGBA data (in black an white).
+            // instead of doing center of mass on it, decide if each pixel is dark enough to consider black or white
+            // then do a center of mass on the thresholded array
+            int[] thresholdedPixels3 = new int[bmp.getWidth()];
+            int wbTotal3 = 0; // total mass
+            int wbCOM3 = 0; // total (mass time position)
+            for (int i = 0; i < bmp.getWidth(); i++) {
+                // sum the red, green and blue, subtract from 255 to get the darkness of the pixel.
+                // if it is greater than some value (600 here), consider it black
+                // play with the 600 value if you are having issues reliably seeing the line
+                if (255*3-(red(pixels3[i])+green(pixels3[i])+blue(pixels3[i])) > 600) {
+                    thresholdedPixels3[i] = 255*3;
+                }
+                else {
+                    thresholdedPixels3[i] = 0;
+                }
+                wbTotal3 = wbTotal3 + thresholdedPixels3[i];
+                wbCOM3 = wbCOM3 + thresholdedPixels3[i]*i;
+            }
+            int COM3;
+            //watch out for divide by 0
+            if (wbTotal3<=0) {
+                COM3 = bmp.getWidth()/2;
+            }
+            else {
+                COM3 = wbCOM3/wbTotal3;
+            }
+
+            // draw a circle where you think the COM is
+            canvas.drawCircle(COM3, startY3, 5, paint1);
+            //****** End of dot 3 ******
+
+            // also write the value as text (top dot)
+            canvas.drawText("COM = " + COM, 10, 200, paint1);
+            c.drawBitmap(bmp, 0, 0, null);
+            mSurfaceHolder.unlockCanvasAndPost(c);
+
+
+
+            // if COM < COM2 (curve angles to the left)
+            // angle = COM2-COM
+            // run right motor faster depending on angle
+
+            // if COM > COM2 (curve angles to right)
+            // angle = COM-COM2
+            // run left motor faster depending on angle
+
+
+
+            // calculate the FPS to see how fast the code is running
+            long nowtime = System.currentTimeMillis();
+            long diff = nowtime - prevtime;
+            mTextView.setText("FPS " + 1000/diff);
+            prevtime = nowtime;
+
+        }
     }
 
 
@@ -189,11 +364,11 @@ public class SerialConsoleActivity extends Activity {
                 //
 
 
-                String sendString = String.valueOf(progress) + "\n";
-                try {
-                    sPort.write(sendString.getBytes(),10); // 10 is the timeout
-                }
-                catch (IOException e) {}
+                //String sendString = String.valueOf(speed) + "\n";
+                //try {
+                //    sPort.write(sendString.getBytes(),10); // 10 is the timeout
+                //}
+                //catch (IOException e) {}
 
             } catch (IOException e) {
                 Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
